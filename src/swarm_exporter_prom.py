2c507f612ea028f1d31e46ac85a605f9a860c1c5
Nicolas DUBUT <nicolas.dubut@lerm.tech>
2025-06-27 12:14:22 +0200

    feat: add services and tasks in exporter

--- src/swarm_exporter_prom.py
+++ src/swarm_exporter_prom.py
@@ -17,7 +17,7 @@
 
 from datetime import datetime, timedelta
 import docker
-from prometheus_client import start_http_server, Counter
+from prometheus_client import start_http_server, Counter, Gauge
 import os
 import platform
 from typing import Any, Optional
@@ -36,7 +36,7 @@ signal.signal(signal.SIGINT, handle_shutdown)
 signal.signal(signal.SIGTERM, handle_shutdown)
 
 
-APP_NAME = "Docker engine networks prometheus exporter"
+APP_NAME = "Docker Swarm prometheus exporter"
 
 PROMETHEUS_EXPORT_PORT = int(os.getenv('PROMETHEUS_EXPORT_PORT', '9000'))
 DOCKER_HOSTNAME = os.getenv('DOCKER_HOSTNAME', platform.node())
@@ -45,7 +45,7 @@ MAX_RETRIES_IN_ROW = int(os.getenv('MAX_RETRIES_IN_ROW', '10'))
 
 DOCKER_SWARM_NODE = Counter(
     'docker_swarm_node',
-    'Total used IPs in network on Host by containers',
+    'Docker Swarm node information',
     [
         'docker_swarm_node_id',
         'docker_swarm_node_spec_role',
@@ -62,6 +62,38 @@ DOCKER_SWARM_NODE = Counter(
     ]
 )
 
+DOCKER_SWARM_SERVICE = Gauge(
+    'docker_swarm_service',
+    'Docker Swarm service information',
+    [
+        'docker_swarm_service_id',
+        'docker_swarm_service_name',
+        'docker_swarm_service_version',
+        'docker_swarm_service_created_at',
+        'docker_swarm_service_updated_at',
+        'docker_swarm_service_mode',
+        'docker_swarm_service_replicas',
+        'docker_swarm_service_image',
+    ]
+)
+
+DOCKER_SWARM_TASK = Gauge(
+    'docker_swarm_task',
+    'Docker Swarm task information',
+    [
+        'docker_swarm_task_id',
+        'docker_swarm_task_name',
+        'docker_swarm_task_service_id',
+        'docker_swarm_task_service_name',
+        'docker_swarm_task_node_id',
+        'docker_swarm_task_state',
+        'docker_swarm_task_desired_state',
+        'docker_swarm_task_created_at',
+        'docker_swarm_task_updated_at',
+        'docker_swarm_task_image',
+    ]
+)
+
 def print_timed(msg):
     to_print = '{} [{}]: {}'.format(
         datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -72,12 +104,18 @@ def print_timed(msg):
 
 def watch_swarm():
     client = docker.DockerClient()
+    # Create an APIClient for low-level API access
+    api_client = docker.APIClient()
 
     try:
         while not exit_event.is_set():
+            # Reset all gauges to avoid stale metrics
+            DOCKER_SWARM_SERVICE._metrics.clear()
+            DOCKER_SWARM_TASK._metrics.clear()
+
+            # Collect node metrics
             nodes = client.nodes.list()
             for node in nodes:
-                
                 attrs = node.attrs
                 DOCKER_SWARM_NODE.labels(
                     **{
@@ -95,16 +133,68 @@ def watch_swarm():
                         'docker_swarm_node_managerstatus_addr': attrs.get('ManagerStatus', {}).get('Addr', ''),
                     }).inc()
 
+            # Collect service metrics
+            services = client.services.list()
+            for service in services:
+                attrs = service.attrs
+                spec = attrs.get('Spec', {})
+                task_template = spec.get('TaskTemplate', {})
+                container_spec = task_template.get('ContainerSpec', {})
+                mode = spec.get('Mode', {})
+                replicated = mode.get('Replicated', {})
+                replicas = replicated.get('Replicas', 0) if replicated else 0
+
+                # If service is in global mode, set replicas to -1 to indicate global
+                if 'Global' in mode:
+                    replicas = -1
+
+                DOCKER_SWARM_SERVICE.labels(
+                    docker_swarm_service_id=attrs.get('ID', ''),
+                    docker_swarm_service_name=spec.get('Name', ''),
+                    docker_swarm_service_version=str(attrs.get('Version', {}).get('Index', '')),
+                    docker_swarm_service_created_at=attrs.get('CreatedAt', ''),
+                    docker_swarm_service_updated_at=attrs.get('UpdatedAt', ''),
+                    docker_swarm_service_mode='global' if 'Global' in mode else 'replicated',
+                    docker_swarm_service_replicas=str(replicas),
+                    docker_swarm_service_image=container_spec.get('Image', ''),
+                ).set(1)  # Set to 1 to indicate the service exists
+
+            # Collect task metrics
+            tasks = api_client.tasks()
+            for task in tasks:
+                # With APIClient, task is already a dictionary
+
+                # Get service name from service ID
+                service_id = task.get('ServiceID', '')
+                service_name = ''
+                for service in services:
+                    if service.id == service_id:
+                        service_name = service.attrs.get('Spec', {}).get('Name', '')
+                        break
+
+                DOCKER_SWARM_TASK.labels(
+                    docker_swarm_task_id=task.get('ID', ''),
+                    docker_swarm_task_name=task.get('Name', ''),
+                    docker_swarm_task_service_id=service_id,
+                    docker_swarm_task_service_name=service_name,
+                    docker_swarm_task_node_id=task.get('NodeID', ''),
+                    docker_swarm_task_state=task.get('Status', {}).get('State', ''),
+                    docker_swarm_task_desired_state=task.get('DesiredState', ''),
+                    docker_swarm_task_created_at=task.get('CreatedAt', ''),
+                    docker_swarm_task_updated_at=task.get('UpdatedAt', ''),
+                    docker_swarm_task_image=task.get('Spec', {}).get('ContainerSpec', {}).get('Image', ''),
+                ).set(1)  # Set to 1 to indicate the task exists
 
             exit_event.wait(SCRAPE_INTERVAL)
     finally:
         client.close()
+        api_client.close()
 
 
 if __name__ == '__main__':
     print_timed(f'Start prometheus client on port {PROMETHEUS_EXPORT_PORT}')
     start_http_server(PROMETHEUS_EXPORT_PORT, addr='0.0.0.0')
-    
+
     failure_count = 0
     last_failure: Optional[datetime] = None
     while not exit_event.is_set():
